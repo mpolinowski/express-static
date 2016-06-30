685de3bfe9da88e9c285b8da10454d818aa81ec5
mpolinowski <mpolinowski@gmail.com>
2016-06-30 20:39:55 +0800

    Added Content

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var redirect = require('express-redirect');
 
 var indexRouter = require('./routes/indexRoutes');
 var searchRouter = require('./routes/searchRoutes');
@@ -29,12 +30,20 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Redirect
+redirect(app);
+/* Quick_Installation */
+/* How does an IP-Camera-Network work? */
+app.redirect('/index.php/How_does_an_IP-Camera-Network_work%3F', '../Quick_Installation/How_Does_An_IP_Camera_Work', 301);
+app.redirect('/index.php/Wie_arbeitet_eine_IP_Kamera%3F', '../Quick_Installation/How_Does_An_IP_Camera_Work', 301);
+
 app.use('/', indexRouter);
 app.use('/Search', searchRouter);
 app.use('/Products', productRouter);
 app.use('/Quick_Installation/', quickInstallRouter);
 app.use('/Internet_Access/', internetAccessRouter);
 app.use('/Motion_Detection/', motionDetectionRouter);
+
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
