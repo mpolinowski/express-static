75f06bdc4867167f10e0762245241f9f1f728383
mpolinowski <mpolinowski@gmail.com>
2016-11-19 16:18:46 +0800

    favicon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -24,9 +23,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
-
-// Uncomment after placing your favicon in /public
-app.use(favicon(path.join (__dirname, 'public', 'favicon.ico')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
