63af5a49b523c5d3b25054a4eaa3c32668a80e78
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-09-28 20:20:57 +0800

    1080p Lenses

--- dev/routes/productRoutes.js
+++ dev/routes/productRoutes.js
@@ -100,6 +100,65 @@ productRouter.route('/Lenses')
     });
 });
 
+/* ****************************************** GET Full HD Lenses ****************************************** */
+productRouter.route('/Lenses/IN-8015/')
+.get(function(req, res) {
+  res.render('./Products/Lenses_IN-8015', {
+      title: 'IN-8015 Full HD Lense Upgrade',
+      ampimg: '/images/Search/P_SearchThumb_Products.jpg',
+      headline: 'INSTAR Products',
+      breadcrumbs: '<li><a href="/">Home</a></li><li><a href="/Products/">Products</a></li><li><a href="/Products/Lenses/">Lenses</a></li><li><a href="/Products/Lenses/IN-8015/">IN-8015 Full HD</a></li>',
+      canonical: '/Products/Lenses/IN-8015/',
+      description: 'INSTAR Wiki - IP Camera Knowledge Base',
+  		pacc: 'expanded',
+  		ptab: '',
+  		qiacc: '',
+  		qitab: 'hidden',
+  		mdacc: '',
+  		mdtab: 'hidden',
+  		iaacc: '',
+  		iatab: 'hidden',
+  		auacc: '',
+  		autab: 'hidden',
+  		hdacc: 'hidden',
+  		hdtab: 'hidden',
+  		fhdacc: 'hidden',
+  		fhdtab: 'hidden',
+  		sacc: 'hidden',
+  		dlacc: 'hidden',
+  		dltab: 'hidden',
+    });
+});
+
+productRouter.route('/Lenses/IN-9008/')
+.get(function(req, res) {
+  res.render('./Products/Lenses_IN-9008', {
+      title: 'IN-9008 Full HD Lense Upgrade',
+      ampimg: '/images/Search/P_SearchThumb_Products.jpg',
+      headline: 'INSTAR Products',
+      breadcrumbs: '<li><a href="/">Home</a></li><li><a href="/Products/">Products</a></li><li><a href="/Products/Lenses/">Lenses</a></li><li><a href="/Products/Lenses/IN-9008/">IN-9008 Full HD</a></li>',
+      canonical: '/Products/Lenses/IN-9008/',
+      description: 'INSTAR Wiki - IP Camera Knowledge Base',
+  		pacc: 'expanded',
+  		ptab: '',
+  		qiacc: '',
+  		qitab: 'hidden',
+  		mdacc: '',
+  		mdtab: 'hidden',
+  		iaacc: '',
+  		iatab: 'hidden',
+  		auacc: '',
+  		autab: 'hidden',
+  		hdacc: 'hidden',
+  		hdtab: 'hidden',
+  		fhdacc: 'hidden',
+  		fhdtab: 'hidden',
+  		sacc: 'hidden',
+  		dlacc: 'hidden',
+  		dltab: 'hidden',
+    });
+});
+
 /* ****************************************** GET S-Mount Lenses ****************************************** */
 productRouter.route('/Lenses/S-Mount/IN-0180')
 .get(function(req, res) {
